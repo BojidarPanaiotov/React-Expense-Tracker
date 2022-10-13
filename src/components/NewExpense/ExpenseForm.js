f408e6983b47bd348905e035830906e0c96b54b8
Bozhidar Panayotov <cccpisthebest@abv.bg>
2022-10-13 12:07:23 +0300

    Child to parent component communication implemented
    New expense component is passing the data to the expense form component (up from the bottom)

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
@@ -44,6 +44,7 @@ const ExpenseForm = () => {
       date: new Date(enteredDate),
     };
 
+    props.onSaveExpenseDate(expenseData);
     setEnteredTitle("");
     setEnteredAmount("");
     setEnteredDate("");
